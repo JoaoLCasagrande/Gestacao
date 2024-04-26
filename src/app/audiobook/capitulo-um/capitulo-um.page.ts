48527676e95332d1da2bebaeeb505f6bcf40e268
João Luiz Casagrande <61015263+JoaoLCasagrande@users.noreply.github.com>
2024-04-25 21:55:08 -0300

    Áudios implementados com sucesso e funcionando
    Os áudios foram implementados no programa, os erros de execução também foram consertados, sendo assim, os áudios estão dando play normalmente.

--- src/app/audiobook/capitulo-um/capitulo-um.page.ts
+++ src/app/audiobook/capitulo-um/capitulo-um.page.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapituloUmPage implements OnInit {
 
+  playAudio() {
+    const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
+    audioPlayer.play();
+  }
+
+  pauseAudio() {
+    const audioPlayer = document.getElementById('audioPlayer') as HTMLAudioElement;
+    audioPlayer.pause();
+  }
+
   constructor() { }
 
   ngOnInit() {
